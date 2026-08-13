4d0c2bd5e47cf637cd9ba2dbe47e7fdd717a411d
Samueru-sama <xdglawyer@outlook.com>
2026-08-13 19:43:04 -0400

    improve workaround for wine issues

--- make-appimage.sh
+++ make-appimage.sh
@@ -17,6 +17,15 @@ export DEPLOY_GSTREAMER=1
 export DEPLOY_VULKAN=1
 export DEPLOY_OPENGL=1
 
+# trick quick-sharun into thinking some binaries are pyinstaller binaries
+# becasue several binaries here cannot be executed via userland-execve
+:> /tmp/.wine-pydata
+for winebin in /usr/bin/wine /usr/bin/wineserver /usr/lib/wine/x86_64-unix/wine; do
+	[ -f "$winebin" ] || continue
+	objcopy --add-section pydata=/tmp/.wine-pydata \
+		--set-section-flags pydata=noload,readonly "$winebin"
+done
+
 # Deploy dependencies
 mkdir -p /tmp/wine
 WINEPREFIX=/tmp/wine quick-sharun \
@@ -35,7 +44,7 @@ WINEPREFIX=/tmp/wine quick-sharun \
 	/usr/lib/libfreetype.so*   \
 	/usr/lib/libharfbuzz*      \
 	/usr/lib/libgraphite*      \
-	/usr/lib/libavcodec.so*	   \
+	/usr/lib/libavcodec.so*    \
 	/usr/lib/libavdevice.so*   \
 	/usr/lib/libavfilter.so*   \
 	/usr/lib/libavformat.so*   \
@@ -52,30 +61,17 @@ WINEPREFIX=/tmp/wine quick-sharun \
 wget --retry-connrefused --tries=30 https://raw.githubusercontent.com/Winetricks/winetricks/master/src/winetricks -O ./AppDir/bin/winetricks
 chmod +x ./AppDir/bin/winetricks
 
-# alright here the pain starts
-ln -sr ./AppDir/lib/wine/x86_64-unix/*.so* ./AppDir/bin
-
-# this gets broken by sharun somehow
-kek=.$(tr -dc 'A-Za-z0-9_=-' < /dev/urandom | head -c 10)
+# quick-sharun replaces lib/wine/x86_64-unix/wine with a
+# sharun hardlink so have to restore the real binay (keep reading to see why)
 rm -f ./AppDir/lib/wine/x86_64-unix/wine
 cp /usr/lib/wine/x86_64-unix/wine ./AppDir/lib/wine/x86_64-unix/wine
-patchelf --set-interpreter /tmp/"$kek" ./AppDir/lib/wine/x86_64-unix/wine
-# we used to run patchelf --add-needed anylinux.so on the wine binary
-# but after 11.8 this causes the binary to break horribly:
-# AppDir/lib/wine/x86_64-unix/wine: oops... not enough space for load commands
-# so we will ahve to make sure anylinux.so loads by adding it as a dependency to the libc
-patchelf --add-needed anylinux.so ./AppDir/shared/lib/libc.so.6
 
-cat <<EOF > ./AppDir/bin/random-linker.src.hook
-#!/bin/sh
-cp -f "\$APPDIR"/shared/lib/ld-linux*.so* /tmp/"$kek"
-EOF
-chmod +x ./AppDir/bin/*.hook
+# it turns out that wine itself performs userland-execve on the wine binary
+# in lib, and it checks its PT_INTERP before doing so, so we have to set it manually
+# to our bundled dynamic linker, which sharun will automatically copy to /tmp
+patchelf --set-interpreter /tmp/.ld-sharun.so.67 ./AppDir/lib/wine/x86_64-unix/wine
 
-# Set the lib path to also use wine libs
-echo 'LD_LIBRARY_PATH=${APPDIR}/lib:${APPDIR}/lib/pulseaudio:${APPDIR}/lib/alsa-lib:${APPDIR}/lib/wine/x86_64-unix' >> ./AppDir/.env
-
-# strip windows libs, inspired by alpine linux: 
+# strip windows libs, inspired by alpine linux:
 # https://gitlab.alpinelinux.org/alpine/aports/-/blob/master/community/wine/APKBUILD
 if [ "$ARCH" = 'x86_64' ]; then
 	x86_64-w64-mingw32-strip -R .comment --strip-unneeded ./AppDir/lib/wine/x86_64-windows/*.dll
